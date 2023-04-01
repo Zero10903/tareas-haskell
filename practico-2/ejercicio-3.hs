418e7ca7b957b7f43daf4736bbd5a3c11337bcef
Zero <samuelbustos10903@gmail.com>
2023-04-01 00:14:39 -0300

    initial commit

---
+++ practico-2/ejercicio-3.hs
@@ -0,0 +1,29 @@
+-- Ejercicio 3.a
+-- soloPares :: [Int]-> [Int], que dada una lista de enteros xs devuelve una lista sólo con los números pares contenidos en xs,
+-- en el mismo orden y con las mismas repeticiones (si las hubiera). Por ejemplo: soloPares [3,0,-2,12] = [0,-2, 12]
+
+soloPares :: [Int]-> [Int]
+
+soloPares [] = []
+soloPares (x:xs) | mod x 2 == 0 = x : soloPares xs
+                 | mod x 2 /= 0 = soloPares xs
+
+-- Ejercicio 3.b
+-- mayoresQue10 :: [Int]-> [Int], que dada una lista de enteros xs devuelve una lista sólo con los números mayores 
+-- que 10 contenidos en xs, Por ejemplo: mayoresQue10 [3,0,-2, 12] = [12]
+
+mayoresQue10 :: [Int]-> [Int]
+
+mayoresQue10 [] = []
+mayoresQue10 (x:xs) | x > 10 = x : mayoresQue10 xs
+                    | x <= 10 = mayoresQue10 xs
+
+-- Ejercicio 3.c
+-- mayoresQue :: Int-> [Int]-> [Int], que dado un entero n y una lista de enteros xs devuelve una lista sólo con los
+-- números mayores que n contenidos en xs, Por ejemplo: mayoresQue 2 [3,0,-2, 12] = [3,12]
+
+mayoresQue :: Int-> [Int]-> [Int]
+
+mayoresQue lim [] = []
+mayoresQue lim (x:xs) | x > lim = x : mayoresQue lim xs
+                      | x <= lim = mayoresQue lim xs
